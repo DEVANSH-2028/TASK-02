ba8b53008445fa603ab65ff0e106645b61cb4456
DEVANSH-2028 <devanshmishracse28@gmail.com>
2025-05-06 12:59:29 +0530

    Update day02task03.py

--- day02task03.py
+++ day02task03.py
@@ -1,3 +1,3 @@
-dic1={"0191cs241111":"harsh agrawal","0191cs241112":"harsh dwivedi","0191cs241113":"harsh gupta"}
+dic1={"0191cs241095":"devansh mishra","0191cs24110":"rom rom  bhaiyon ","0191cs24109":"new gupta"}
 j=input("enter the roll number")
 print(dic1.get(j))
